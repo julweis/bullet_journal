9fe9a5f04ae52572790a2a61b0202afe4f1be075
Julia Weis <julweis@users.noreply.github.com>
2016-12-09 13:22:13 -0600

    Final version

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root :to => "tasks#index"
+  root :to => "today#index"
 
 # Route for the TODAY view:
 get "/today", :controller => "today", :action=> "index"
